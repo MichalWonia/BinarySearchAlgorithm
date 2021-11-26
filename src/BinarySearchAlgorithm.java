b402a7c8140e806a0760422ed60423d71e80a51f
Michal Wonia <spodek.mw@gmail.com>
2021-11-26 10:28:25 +0100

    Add necessary variables

--- src/BinarySearchAlgorithm.java
+++ src/BinarySearchAlgorithm.java
@@ -2,5 +2,12 @@ public class BinarySearchAlgorithm {
 
     public static void main(String[] args) {
 
+        int[] sortedIntArray = {1, 13, 14, 67, 138, 149, 221};
+        int indexOfFoundElement = 1; // -1 means that the element was not found
+        int min_index = 0;
+        int max_index = sortedIntArray.length - 1;
+        int mid_index = (min_index + max_index) / 2;
+
+
     }
 }
